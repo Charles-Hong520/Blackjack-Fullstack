ba778dabb4db2c7df6c63e624fe6ce238b9bfa60
Gunwoo Kim <gunwoolouie@gmail.com>
2024-07-03 23:01:39 -0700

    dockerized flask server.

--- server/src/server.py
+++ server/src/server.py
@@ -1,24 +1,10 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, send, emit
-# from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
-socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
 
-game_state = {}
-
-@app.route('/api', methods=['GET'])
-def index():
-    return "Game API"
-
-@socketio.on('player_move')
-def handle_player_move(move):
-    # Update game state based on the move
-    global game_state
-    game_state['move'] = move
-    print('move', move)
-    emit('game_state', game_state, broadcast=True)
+@app.route('/')
+def hello():
+    return "Hello, Dockerized Flask App!"
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False,allow_unsafe_werkzeug=True)
+    app.run(debug=True, host='0.0.0.0')
